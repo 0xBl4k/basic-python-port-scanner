d2660d1a098a61ccd43abe1db6be439f92628b80
0xBl4k <halilgrarrar@gmail.com>
2026-07-12 22:57:57 +0300

    improved the scanner with error handling still needs improvments because its slow

--- port_scanner.py
+++ port_scanner.py
@@ -1,19 +1,27 @@
 import socket
-
+import time
 # testing host is scanme.nmap.org
 
-#HOST = socket.gethostbyname(socket.gethostname())
-HOST = 'scanme.nmap.org'
-print(f"Target: {HOST}")
-print("-"*40)
 try:    
-    for PORT in range(1, 201):
-        scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        scan.settimeout(5)
-        code = scan.connect_ex((HOST, PORT)) 
-        if code ==  0:
-            print(f"Port {PORT} is open")
+    HOST = socket.gethostbyname('scanme.nmap.org')
+    print(f"Target: {HOST}")
+    print(f"Start Time: {time.strftime("%m/%d/%Y %I:%M %p")}")
+    print("-"*40)
+
+    for PORT in range(1, 65535):
+            scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            scan.settimeout(1)
+            try:
+                code = scan.connect_ex((HOST, PORT)) 
+                if code ==  0:
+                    print(f"Open port at: {PORT}")
+            except TimeoutError:
+                 pass
+            scan.close()
+
 except KeyboardInterrupt:
-    print("You Stopped The Program") 
+    print("You Stopped The Program")
+except socket.gaierror:
+    print("Name or Service not known")
        
 
